0a9585f381f2de9222f5ab1e8cfc4cb1a37dde76
cnopt <cbiddisc@gmail.com>
2022-03-23 22:47:20 +0000

    metro, fuel, paypal

---
+++ all transactions at PETROL STATIONS.sql
@@ -0,0 +1,8 @@
+SELECT Transaction_Description as 'desc', Transaction_Date as 'date', Debit_Amount as 'amount'
+FROM [dbo].[statement-2022-dateformatted] 
+WHERE Transaction_Date BETWEEN '2014-01-01' AND '2023-12-31'
+    AND Transaction_Description LIKE '%PETROL%' 
+    OR Transaction_Description LIKE '%SHELL%'
+AND Debit_Amount IS NOT NULL
+ORDER BY Debit_Amount DESC
+-- ORDER BY Transaction_Date
