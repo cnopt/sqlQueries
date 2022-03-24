51dbc8ce90416f109c1cdb990617ac8026553195
cnopt <cbiddisc@gmail.com>
2022-03-24 17:19:58 +0000

    modified last file to display month numbers and be ordered by month, then day

--- get all debit transactions in 2021 grouped by month and day.sql
+++ get all debit transactions in 2021 grouped by month and day.sql
@@ -1,5 +1,11 @@
-SELECT MONTH(Transaction_Date) AS 'Month', DAY(Transaction_date) AS 'Day', Debit_Amount
-  FROM [dbo].[statement-2022-dateformatted]
-  WHERE Debit_Amount IS NOT NULL 
-  AND Transaction_Date BETWEEN '2021-01-01' AND '2021-12-31'
-  ORDER BY Transaction_Date ASC
+  SELECT   FORMAT(Transaction_Date, 'MMMM') AS 'month', 
+           DAY(Transaction_date) AS 'day', 
+           SUM(Debit_Amount) as 'debit_amount'
+  FROM     [dbo].[statement-2022-dateformatted]
+  WHERE    Debit_Amount IS NOT NULL 
+             AND Transaction_Date BETWEEN '2021-01-01' AND '2021-12-31'
+  GROUP BY MONTH(Transaction_Date),
+           FORMAT(Transaction_Date, 'MMMM'),
+           DAY(Transaction_date)
+  ORDER BY MONTH(Transaction_Date),
+           DAY(Transaction_Date)
