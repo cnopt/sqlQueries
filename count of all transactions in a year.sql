17c6b859dd76bd572b000733c8cafec77c829547
cnopt <cbiddisc@gmail.com>
2022-03-24 17:28:15 +0000

    changed to display sum of debit counts in 2014 to 2022

--- count of all transactions in a year.sql
+++ count of all transactions in a year.sql
@@ -2,5 +2,6 @@ SELECT YEAR(Transaction_Date) as 'year',
        COUNT(Debit_Amount) as 'count'
   FROM [dbo].[statement-2022-dateformatted]
   WHERE Debit_Amount IS NOT NULL 
-  AND Transaction_Date BETWEEN '2020-01-01' AND '2020-12-31'
+  AND Transaction_Date BETWEEN '2010-01-01' AND '2050-12-31'
   GROUP BY YEAR(Transaction_Date)
+  ORDER BY YEAR(Transaction_Date)
