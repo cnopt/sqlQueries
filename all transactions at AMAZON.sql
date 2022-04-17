bd2ca1f0f6078f892b17176e441a7adb805265fe
cnopt <cbiddisc@gmail.com>
2022-04-17 12:01:18 +0100

    add union to append total amount to bottom of tables

--- all transactions at AMAZON.sql
+++ all transactions at AMAZON.sql
@@ -1,7 +1,25 @@
-SELECT Transaction_Description as 'desc', Transaction_Date as 'date', Debit_Amount as 'amount'
-FROM [dbo].[statement-2022-dateformatted] 
-WHERE Transaction_Date BETWEEN '2014-01-01' AND '2023-12-31'
-    AND Transaction_Description LIKE '%AMAZON%'
-    OR Transaction_Description LIKE '%AMZN'
-AND Debit_Amount IS NOT NULL
-ORDER BY 'date' ASC
+SELECT *
+FROM ( SELECT TOP 10 Transaction_Description as 'desc',
+              Transaction_Date as 'date',
+              Debit_Amount
+       FROM  [dbo].[statement-2022-dateformatted] 
+       WHERE Transaction_Date BETWEEN '2014-01-01' AND '2023-12-31'
+            AND Debit_Amount IS NOT NULL
+            AND Transaction_Description LIKE '%AMAZON%'
+            OR Transaction_Description LIKE '%AMZN'
+            ORDER BY Debit_Amount DESC )
+AS a
+
+UNION ALL
+
+SELECT 'Total', NULL, SUM(Debit_Amount)
+FROM ( SELECT TOP 10 Transaction_Description as 'desc',
+              Transaction_Date as 'date',
+              Debit_Amount
+       FROM  [dbo].[statement-2022-dateformatted] 
+       WHERE Transaction_Date BETWEEN '2014-01-01' AND '2023-12-31'
+            AND Debit_Amount IS NOT NULL
+            AND Transaction_Description LIKE '%AMAZON%'
+            OR Transaction_Description LIKE '%AMZN'
+            ORDER BY Debit_Amount DESC)
+AS b 
